fb8fa1b6e893c589796e425f408682e6e40ddce6
Noushad <noushadozi333@gmail.com>
2025-01-08 19:50:01 +0600

    deploy

--- pages/category/[id]/[subcatId].js
+++ pages/category/[id]/[subcatId].js
@@ -56,7 +56,6 @@ export async function getStaticProps({ params }) {
     props: {
       cats,
       subcats,
-      duasName,
       duas,
       querycat: params.id,
       querySubcat: params.subcatId,
