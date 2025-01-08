a702c1ad066cd5633d239223fd718527f9e8b38b
Noushad <noushadozi333@gmail.com>
2025-01-08 20:00:05 +0600

    bug fix

--- components/btns/SubCategoryButton.js
+++ components/btns/SubCategoryButton.js
@@ -4,17 +4,16 @@ import { useRouter } from "next/router";
 
 const SubcatButton = ({ subcat, querycat, duas }) => {
   const router = useRouter();
-  
+  const firstDuaOfSubcat = duas.filter(
+    (dua) => dua.subcat_id === subcat.subcat_id
+  )[0].dua_id;
+
   const handleClick = () => {
     router.push(`/category/${querycat}/${subcat.id}`);
   };
 
   return (
-    <a
-      href={`#dua-card-${
-        duas.filter((dua) => dua.subcat_id === subcat.subcat_id)[0].dua_id
-      }`}
-    >
+    <a href={`#dua-card-${firstDuaOfSubcat}`}>
       <div className="py-[8px] relative cursor-pointer" onClick={handleClick}>
         <p className="pl-[19px]">{subcat.subcat_name_en}</p>
         <div className="size-[8px] bg-[#1FA45B] absolute top-4 rounded-full -left-[5px]"></div>
